f3117064f62ca59a6318912d195af85c50278128
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-06-14 15:56:10 -0500

    feat: :sparkles: Informacion del detalle del producto

--- js/Detail.js
+++ js/Detail.js
@@ -1,14 +1,20 @@
 import { galleryCategory } from "./components/gallery.js";
+import { titleProductDetail } from "./components/section.js";
 import { getProductId } from "./module/detail.js";
 
-let main__section_gallery = document.querySelector("#main__section_gallery");
+let main__section_gallery = document.querySelector("#main__section__gallery");
+let main__section__title = document.querySelector("#main__section__title");
+
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
-    main__section_gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
-    // let {data} = res;
+
+    let info = JSON.parse(localStorage.getItem(id));
+    main__section_gallery.innerHTML = await galleryCategory(info);
+    main__section__title.innerHTML = await titleProductDetail(info);
+    
     // let {
     //     category_path,
     //     about_product,
