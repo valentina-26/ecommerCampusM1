00347c9857fb1ca882c27b8cee68379e57924fc4
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-06-24 18:24:53 -0500

    chore: :truck: cambio de headers

--- js/components/env.js
+++ js/components/env.js
@@ -1,3 +1,3 @@
 export const headers = {
-  'x-rapidapi-key': '9ac10b35a9mshaa5cf2f91b91a77p14ec67jsne84c61139ded',
-		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'}
+  'x-rapidapi-key': '9420ab8194msha2fd15fd05d5419p1b198djsn66b61560e711',
+	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'}
--- js/module/app.js
+++ js/module/app.js
@@ -19,7 +19,7 @@ export const getAllCategory = async()=>{
     const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '9ac10b35a9mshaa5cf2f91b91a77p14ec67jsne84c61139ded',
+		'x-rapidapi-key': '9420ab8194msha2fd15fd05d5419p1b198djsn66b61560e711',
 		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 	}
 };
